c781cfe7d9d169ea5c3049736b93132dac1d9586
Ketan Madral <88777696+Ketanmadral@users.noreply.github.com>
2021-08-27 14:30:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var count=0;
+     function addCount(){
+        count++;
+        document.getElementById("result").innerHTML= count;
+      }
+      
+      function lowerCount(){
+        count--;
+        document.getElementById("result").innerHTML = count;
+      }
